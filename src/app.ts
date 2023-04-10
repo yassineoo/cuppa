389be3b3ed2404b084bcc6bf21966bf4c2a83663
yassin <y.attuo@esi-sba.dz>
2023-04-10 21:35:00 +0200

    refactor : preparing for merge

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import loginRoute from './services/authService/routes/auth.Route';
-import paymentRoute from './services/paymentService/routes/payment.Route';
+import  loginRoute from './services/authService/routes/auth.Route';
 import bodyParser from 'body-parser';
-import stripe from './services/paymentService/paymentConfig';
-import Stripe from 'stripe';
+import paymentRoute from './services/paymentService/routes/payment.Route';
+import parser from './middlewares/parser';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use('/payment2/webhook', bodyParser.raw({type: '*/*'}));
-
-
 // Use JSON parser for all non-webhook routes
-app.use(
-	(
-		req: express.Request,
-		res: express.Response,
-		next: express.NextFunction
-	): void => {
-		if (req.originalUrl === '/payment/webhook') {
-			next();
-			console.log('hiiiii webhook');
-			
-		} else {
-			console.log('hiiiii other');
-			express.json()(req, res, next);
-		}
-	}
-);
+app.use(parser);
   
 
 //app.use(express.json());
