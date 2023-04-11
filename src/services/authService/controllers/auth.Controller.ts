dddbeacd53d89b129e21d322351a0ff7205bd01d
yassin <y.attuo@esi-sba.dz>
2023-04-12 00:15:01 +0200

    merge : conflict resolve

--- src/services/authService/controllers/auth.Controller.ts
+++ src/services/authService/controllers/auth.Controller.ts
@@ -43,7 +43,6 @@ class LoginController {
 
 			// Attempt to login and retrieve a JWT token
 			const token = await Authentication.login(loginData);
-			console.log('it passed ----------');
 			
 			// Send the token back in the response
 			res.status(200);
