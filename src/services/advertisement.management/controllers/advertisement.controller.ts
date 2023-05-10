fa4c8571d332867df4b14de5dd0657ea5e13926f
syrine <js_zitouni@esi.dz>
2023-05-10 23:47:55 +0100

    feat : update advertisement management service

--- src/services/advertisement.management/controllers/advertisement.controller.ts
+++ src/services/advertisement.management/controllers/advertisement.controller.ts
@@ -3,43 +3,47 @@ import { Request, Response } from 'express';
 
 import AdvertisementManagmentService from './../service/advertisement.management';
 
-import multer from 'multer';
+import { IncomingForm } from 'formidable';
 
-// Set storage engine for multer
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-	  cb(null, "uploads/");
-	},
-	filename: (req, file, cb) => {
-	  cb(null, `${Date.now()}_${file.originalname}`);
-	},
-  });
+
 
 
 const advertisementService = new AdvertisementManagmentService();
 
 
+// Advertiser controllers
 
 export const createAdvertiser = async (req: Request, res: Response) => {
 	try {
-
-	  //Here is an example of how to create an advertiser
-	  /*
-	  const base64Image = "AAAAHGZ0eXBhdmlmAAAAAGF2aWZta........";
-	  req.body = {
-		nom_annonceur:"Zitouni"	,prenom_annonceur:"Assia"	,sexe_annonceur:"F", image: `data:image/png;base64,${base64Image}`
-	  }
-	  */
-
-	  const advertiser = await advertisementService.createAdvertiser(req.body);
-	  res.status(201).json(advertiser);
+	  const form = new IncomingForm({
+		multiples: false,
+		keepExtensions: true,
+		uploadDir: process.env.UPLOAD_DIR,
+		type: 'multipart',
+		parse: (req, options, callback) => {
+		  // use the default parser
+		  return IncomingForm.prototype.parse.call(this, req, options, callback);
+		}
+	  });
+  
+	  form.parse(req, async (err, fields, files) => {
+		if (err) {
+		  console.error(err);
+		  throw err;
+		}
+  
+		const { ...data } = fields; // destructure the fields
+		const image = files.image ? files.image.filepath : null; // get the path to the image file
+		const advertiser = await advertisementService.createAdvertiser(data, image);
+  
+		res.status(201).json(advertiser);
+	  });
 	} catch (error) {
-	  console.log(error);
-	  res.status(500).json({ message: 'Failed to create advertiser' });
+	  console.error(error);
+	  res.status(500).send({ error: 'Internal server error' });
 	}
-};
-
-// Advertiser controllers
+  };
+  
 
 export const getAllAdvertisers = async (req: Request, res: Response) => {
 	try {
@@ -66,23 +70,39 @@ export const getAllAdvertisers = async (req: Request, res: Response) => {
 	}
   };
   
+  
   export const updateAdvertiser = async (req: Request, res: Response) => {
-	const id = parseInt(req.params.id);
-	
-	req.body = {
-		prenom_annonceur:"Syrine"
-	  }
 	try {
-	  const updatedAdvertiser = await advertisementService.updateAdvertiser(
-		id,
-		req.body
-	  );
-	  res.status(200).json(updatedAdvertiser);
+	  const form = new IncomingForm({
+		multiples: false,
+		keepExtensions: true,
+		uploadDir: process.env.UPLOAD_DIR,
+		type: 'multipart',
+		parse: (req, options, callback) => {
+		  // use the default parser
+		  return IncomingForm.prototype.parse.call(this, req, options, callback);
+		}
+	  });
+  
+	  form.parse(req, async (err, fields, files) => {
+		if (err) {
+		  console.error(err);
+		  throw err;
+		}
+  
+		const { ...data } = fields; // destructure the fields
+		const image = files.image ? files.image.filepath : null; // get the path to the image file
+		const id = parseInt(req.params.id);
+		const advertiser = await advertisementService.updateAdvertiser(id, data, image);
+  
+		res.status(200).json(advertiser);
+	  });
 	} catch (error) {
-	  console.log(error);
-	  res.status(500).json({ message: 'Failed to update advertiser' });
+	  console.error(error);
+	  res.status(500).send({ error: 'Internal server error' });
 	}
   };
+  
   
   export const deleteAdvertiser = async (req: Request, res: Response) => {
 	const id = parseInt(req.params.id);
@@ -95,28 +115,38 @@ export const getAllAdvertisers = async (req: Request, res: Response) => {
 	}
   };
 
-// Init multer upload
-const upload = multer({ storage: storage }).single("video");
 
-export const createAdvertisement = async (req: Request, res: Response) => {
+  export const createAdvertisement = async (req: Request, res: Response) => {
 	try {
-		upload(req, res, async (err) => {
-		  if (err) {
-			console.log(err);
-			return res.status(400).send({ error: "Error uploading file" });
-		  }
-	
-		  const data = req.body;
-	
-		  const annonce = await createAdvertisement(data, (req as any).file);
-	
-		  return res.status(201).send({ annonce });
-		});
-	  } catch (error) {
-		console.log(error);
-		return res.status(500).send({ error: "Internal Server Error" });
-	  }
-};
+		const form = new IncomingForm({
+			multiples: true,
+			keepExtensions: true,
+			uploadDir: process.env.UPLOAD_DIR,
+			type: 'multipart',
+			parse: (req, options, callback) => {
+			   //use the default parser
+			   return IncomingForm.prototype.parse.call(this, req, options, callback);
+			}
+		 });
+		 
+  
+	  form.parse(req, async (err, fields, files) => {
+		if (err) {
+		  console.error(err);
+		  throw err;
+		}
+  
+		const data = { ...fields, videoFile: files.videoFile };
+		//console.log(data.videoFile.filepath);
+		const advertisement = await advertisementService.createAdvertisement(data);
+  
+		res.status(201).json(advertisement);
+	  });
+	} catch (error) {
+	  console.error(error);
+	  res.status(500).send({ error: 'Internal server error' });
+	}
+  };
 
 export const getAllAdvertisements = async (req: Request, res: Response) => {
   try {
@@ -145,20 +175,39 @@ export const getAdvertisementById = async (req: Request, res: Response) => {
 	
 };
 	
-	export const updateAdvertisement = async (req: Request, res: Response) => {
-	const id = parseInt(req.params.id);
+ 
+ export const updateAdvertisement = async (req: Request, res: Response) => {
 	try {
-	const updatedAdvertisement = await advertisementService.updateAdvertisement(
-	id,
-	req.body,
-	req.file
-	);
-	res.status(200).json(updatedAdvertisement);
+	  const { id } = req.params;
+	  const form = new IncomingForm({
+		multiples: true,
+		keepExtensions: true,
+		uploadDir: process.env.UPLOAD_DIR,
+		type: 'multipart',
+		parse: (req, options, callback) => {
+		  //use the default parser
+		  return IncomingForm.prototype.parse.call(this, req, options, callback);
+		}
+	  });
+  
+	  form.parse(req, async (err, fields, files) => {
+		if (err) {
+		  console.error(err);
+		  throw err;
+		}
+  
+		const data = { ...fields, videoFile: files.videoFile };
+		const updatedAdvertisement = await advertisementService.updateAdvertisement(id, data, files.videoFile);
+  
+		res.status(200).json(updatedAdvertisement);
+	  });
 	} catch (error) {
-	console.log(error);
-	res.status(500).json({ message: 'Failed to update advertisement' });
+	  console.error(error);
+	  res.status(500).send({ error: 'Internal server error' });
 	}
-	};
+  };
+  
+  
 	
 	export const deleteAdvertisement = async (req: Request, res: Response) => {
 	const id = parseInt(req.params.id);
--- src/services/advertisement.management/service/advertisement.management.ts
+++ src/services/advertisement.management/service/advertisement.management.ts
@@ -1,8 +1,9 @@
 import Models from './../../../models/sequelize';
 const fs = require('fs');
 const path = require('path');
+import stream from 'stream';
 
-import multer from 'multer';
+import { createReadStream,createWriteStream } from 'fs';
 
 
 
@@ -11,120 +12,108 @@ import multer from 'multer';
 // Initialize the models
 const Annonce = Models.annoce;
 const Annonceur = Models.annoceur;
-
+   
 
 // Define the association between the models
 Annonce.belongsTo(Annonceur, { foreignKey: 'id_annonceur' });
 
 class AdvertisementManagmentService {
 
-	 // create an advertiser
-	 async createAdvertiser(data) {
-		try {
-		  const { image, ...rest } = data; // destructure the image from the data
-		  const advertiser = await Annonceur.create(rest);
-	  
-		  // create the uploads folder if it doesn't exist
-		  const uploadsPath = path.join(__dirname, '..', '..', '..', 'uploads');
-		  if (!fs.existsSync(uploadsPath)) {
-			fs.mkdirSync(uploadsPath);
-		  }
-	  
-		  // save the image to disk
-		  if (image) {
-			const base64Image = image.split(';base64,').pop(); // remove the data:image/png;base64 prefix
-			const imageName = `${advertiser.id_annonceur}.png`; // use the advertiser ID as the image name
-			const imagePath = path.join(uploadsPath, imageName); // specify the path to save the image
-			await fs.promises.writeFile(imagePath, base64Image, { encoding: 'base64' });
-			await advertiser.update({ path_annonceur: imageName });
-		  }
-	  
-		  return advertiser;
-		} catch (error) {
-		  console.log(error);
-		  throw error;
-		}
+	   // create an advertiser
+  async createAdvertiser(data, image) {
+	try {
+	  const { ...rest } = data; // destructure the data
+	  const advertiser = await Annonceur.create(rest);
+  
+	  // create the uploads folder if it doesn't exist
+	  const uploadsPath = path.join(__dirname, '..', '..', '..', 'uploads');
+	  if (!fs.existsSync(uploadsPath)) {
+		fs.mkdirSync(uploadsPath);
 	  }
+  
+	  // save the image to disk
+	  if (image) {
+		const imageName = `advertiser${advertiser.id_annonceur}.png`; // use the advertiser ID as the image name
+		const imagePath = path.join(uploadsPath, imageName); // specify the path to save the image
+  
+		// create a writable stream to write the image to disk
+		const imageStream = fs.createWriteStream(imagePath);
+  
+		// create a readable stream from the image data and pipe it to the writable stream
+		const readableImageStream = stream.Readable.from(image);
+		readableImageStream.pipe(imageStream);
+  
+		await advertiser.update({ path_annonceur: imagePath });
+	  }
+  
+	  return advertiser;
+	} catch (error) {
+	  console.log(error);
+	  throw error;
+	}
+  }
+  
 	  
 		// get all advertisers
 		async getAllAdvertisers() {
 			try {
-			  const advertisers = await Annonceur.findAll();
-			  const advertisersWithImages = await Promise.all(advertisers.map(async advertiser => {
-				const imagePath = path.join(__dirname, '..', '..', '..', 'uploads', advertiser.path_annonceur);
-				const imageBuffer = await fs.promises.readFile(imagePath);
-				
-				return {
-				  id_annonceur: advertiser.id_annonceur,
-				  nom_annonceur: advertiser.nom_annonceur,
-				  prenom_annonceur: advertiser.prenom_annonceur,
-				  sexe_annonceur: advertiser.sexe_annonceur,
-				  path_annonceur: advertiser.path_annonceur,
-				  image: `data:image/png;base64,${imageBuffer.toString('base64')}`
-				};
-			  }));
-		
-			  return advertisersWithImages;
+			  const annonceurs = await Annonceur.findAll();
+			  return annonceurs;
 			} catch (error) {
 			  console.log(error);
 			  throw error;
 			}
-		}
+		  }
 		
 	
 		// get an advertiser by id
 		async getAdvertiserById(id) {
 			try {
-			  const advertiser = await Annonceur.findByPk(id);
-			  if (!advertiser) {
-				throw new Error('Advertiser not found');
+				const annonceur = await Annonceur.findOne({
+				  where: { id_annonceur: id }
+				});
+				return annonceur;
+			  } catch (error) {
+				console.log(error);
+				throw error;
 			  }
-		
-			  const imagePath = path.join(__dirname, '..', '..', '..', 'uploads', advertiser.path_annonceur);
-			  const imageBuffer = await fs.promises.readFile(imagePath);
-		
-			  return {
-				id_annonceur: advertiser.id_annonceur,
-				nom_annonceur: advertiser.nom_annonceur,
-				prenom_annonceur: advertiser.prenom_annonceur,
-				sexe_annonceur: advertiser.sexe_annonceur,
-				path_annonceur: advertiser.path_annonceur,
-				image: `data:image/png;base64,${imageBuffer.toString('base64')}`
-			  };
-			} catch (error) {
-			  console.log(error);
-			  throw error;
-			}
 		  }
 		
 	
 		
-		// update an advertiser by id
-		async updateAdvertiser(id, data) {
-			try {
-			  const { image, ...rest } = data;
-			  const advertiser = await Annonceur.findOne({ where: { id_annonceur: id } });
-			  if (!advertiser) {
-				throw new Error(`Advertiser with id ${id} not found`);
-			  }
-		  
-			  await advertiser.update(rest);
-		  
-			  if (image) {
-				const base64Image = image.split(';base64,').pop();
-				const imageName = `${advertiser.id_annonceur}.png`;
-				const imagePath = path.join(__dirname, '..', '..', '..', 'uploads', imageName);
-				await fs.promises.writeFile(imagePath, base64Image, { encoding: 'base64' });
-				await advertiser.update({ path_annonceur: imageName });
-			  }
-		  
-			  return advertiser.toJSON();
-			} catch (error) {
-			  console.log(error);
-			  throw error;
-			}
-		  }
-		  
+	// update an advertiser by id
+   async updateAdvertiser(id, data, image) {
+	try {
+	  const { ...rest } = data;
+	  const advertiser = await Annonceur.findOne({ where: { id_annonceur: id } });
+	  if (!advertiser) {
+		throw new Error(`Advertiser with id ${id} not found`);
+	  }
+	
+	  await advertiser.update(rest);
+	
+	  // save the image to disk
+	  if (image) {
+		const imageName = `advertiser${advertiser.id_annonceur}.png`; // use the advertiser ID as the image name
+		const imagePath = path.join(__dirname, '..', '..', '..', 'uploads', imageName); // specify the path to save the image
+	
+		// create a writable stream to write the image to disk
+		const imageStream = fs.createWriteStream(imagePath);
+	
+		// create a readable stream from the image data and pipe it to the writable stream
+		const readableImageStream = stream.Readable.from(image);
+		readableImageStream.pipe(imageStream);
+	
+		await advertiser.update({ path_annonceur: imagePath });
+	  }
+	
+	  return advertiser.toJSON();
+	} catch (error) {
+	  console.log(error);
+	  throw error;
+	}
+  }
+  
 		
          // Delete an advertiser by ID
     async deleteAdvertiser(id) {
@@ -134,7 +123,7 @@ class AdvertisementManagmentService {
 		throw new Error(`Advertiser with id ${id} not found`);
 	  }
   
-	  const imagePath = path.join(__dirname, '..', '..', '..', 'uploads', advertiser.path_annonceur);
+	  const imagePath = advertiser.path_annonceur;
 	  await fs.promises.unlink(imagePath); // delete the image file
   
 	  await advertiser.destroy(); // delete the advertiser from the database
@@ -149,28 +138,55 @@ class AdvertisementManagmentService {
 
 
 
-   // create a new advertisement
-   async createAdvertisement(data, videoFile) {
+ // createAdvertisement method
+ async createAdvertisement(data) {
 	try {
-	  const { id_annonceur } = data;
+	  const {
+		id_annonceur,
+		duree_affichage,
+		ageMin,
+		ageMax,
+		sexeCible,
+		prix_annonce,
+		path_video,
+		videoFile
+	  } = data;
+  
 	  const advertiser = await Annonceur.findOne({ where: { id_annonceur } });
 	  if (!advertiser) {
 		throw new Error(`Annonceur with id ${id_annonceur} not found`);
 	  }
   
 	  // save the uploaded video file to the uploads directory
-	  const videoPath = videoFile.path;
+	  const uploadsPath = path.join(__dirname, '..', '..', '..', 'uploads');
+	  if (!fs.existsSync(uploadsPath)) {
+		fs.mkdirSync(uploadsPath);
+	  }
+  
+	  // generate the new video file name using the auto-incremented ID of the Annonce instance
+	  const annonce = await Annonce.create({ id_annonceur, duree_affichage, ageMin, ageMax, sexeCible, prix_annonce, path_video });
+	  const videoName = `advertisement${annonce.id_annonce}.mp4`;
+  
+	  const videoPath = path.join(uploadsPath, videoName);
+	  console.log(videoName);
+  
+	  // Saving the video to the server
+	  await createReadStream(videoFile.filepath).pipe(createWriteStream(videoPath));
   
 	  // add the video path to the advertisement data
-	  const dataWithVideo = { ...data, path_video: videoPath };
+	  const dataWithVideo = { id_annonceur, duree_affichage, ageMin, ageMax, sexeCible, prix_annonce, path_video: videoPath };
   
-	  const annonce = await Annonce.create(dataWithVideo);
+	  await annonce.update(dataWithVideo);
 	  return annonce;
 	} catch (error) {
-	  console.log(error);
+	  console.error(error);
 	  throw error;
 	}
-  }
+  };
+  
+	  
+  
+  
   
 
   // get all advertisements
@@ -201,27 +217,36 @@ class AdvertisementManagmentService {
   }
 
  // Update an advertisement by ID
-  async updateAdvertisement(id, data, videoFile) {
+ async updateAdvertisement(id, data, videoFile) {
 	try {
 	  const advertisement = await Annonce.findOne({ where: { id_annonce: id } });
 	  if (!advertisement) {
 		throw new Error(`Advertisement with id ${id} not found`);
 	  }
-	  // save the uploaded video file to the uploads directory
-     const videoPath = videoFile.path;
-
-     // add the video path to the advertisement data
-     const dataWithVideo = { ...data, path_video: videoPath };
-
-     const updatedAdvertisement = { ...advertisement.toJSON(), ...dataWithVideo };
-     await advertisement.update(updatedAdvertisement);
-     return advertisement.toJSON();
-
+  
+	  // Save the uploaded video file to the uploads directory
+	  const uploadsPath = path.join(__dirname, '..', '..', '..', 'uploads');
+	  if (!fs.existsSync(uploadsPath)) {
+		fs.mkdirSync(uploadsPath);
+	  }
+	  const videoName = `advertisement${id}.mp4`;
+	  const videoPath = path.join(uploadsPath, videoName);
+	  await createReadStream(videoFile.filepath).pipe(createWriteStream(videoPath));
+  
+	  // Add the video path to the advertisement data
+	  const dataWithVideo = { ...data, path_video: videoPath };
+  
+	  // Update the advertisement
+	  const updatedAdvertisement = { ...advertisement.toJSON(), ...dataWithVideo };
+	  await advertisement.update(updatedAdvertisement);
+  
+	  return advertisement.toJSON();
 	} catch (error) {
 	  console.log(error);
 	  throw error;
 	}
   }
+  
   
 
  // Delete an advertisement by ID
